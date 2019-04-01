a38fce18b9efe5d4045ead26e21757989583025c
otavio25 <souzaotavio721@gmail.com>
2019-04-01 16:36:06 -0300

    Projeto de Front-End

---
+++ USA/js/javascript.js
@@ -0,0 +1,79 @@
+$(document).ready(function () {
+    $("#a1").click(function () {
+        $("#carousel1").attr("src", "img/CH1.jpg");
+        $("#carousel2").attr("src", "img/CH2.jpg");
+        $("#carousel3").attr("src", "img/CH3.jpg");
+        $("#carousel4").attr("src", "img/CH4.jpg");
+        $("#title").text("The Windy City !!!");
+        //$(".imgTitle").attr("src","img/titleCH.png");
+
+        $("#pt1").attr("src", "img/ptch1.jpg");
+        $("#titlePT1").text("Chicago River");
+        $("#pontosTuristicos1").text("O rio de Chicago, o qual conta com águas bem esverdeadas, oferece um passeio lindo com fotos incríveis. Nossa dica é para que você passeie pela margem desta atração, parando nos cafés regionais e observando a paisagem e arquitetura de Chicago. Faça um passeio de barco ou caiaque, pare para sentar-se e tomar sol em uma das cadeiras por lá e, ainda, andar de bicicleta. A magia deste ponto turístico de Chicago é que ele é lindo em todos os períodos do dia, com visões diferentes em cada turno.");
+
+        $("#pt2").attr("src", "img/ptch2.jpg");
+        $("#titlePT2").text("Millennium Park");
+        $("#pontosTuristicos2").text("O Millennium Park de Chicago é um belo ponto turístico, o qual você não pode perder por nada. Saiba que esta atração conta com diversas paisagens icônicas, como as várias obras de arte e a fonte de água Crown Fountain, a qual forma imagens fantásticas. Outra coisa que você encontra neste parque é a escultura Cloud Gate, a qual é, sem dúvidas, um dos maiores pontos turísticos de Chicago, tratando-se daquela escultura espelhada, que se assemelha a um feijão. Além disso, é possível ter um passeio muito agradável neste parque, o qual é cheio de flores lindas e que criam um frescor neste local.");
+
+        $("#pt3").attr("src", "img/ptch3.jpg");
+        $("#titlePT3").text("Navy Pier");
+        $("#pontosTuristicos3").text("Este lugar de Chicago é um ponto turístico que atende todos os gostos e bolsos. Para você ter uma ideia, por lá existe o famoso museu da criança, parque de diversões, cinema i-Max e museu de Shakespeare, os quais são ótimos e possuem um valor de entrada. Este é o caso, também, dos passeios de barco no lago Michigan de Chicago e das bicicletas, que estão disponíveis para aluguel no Navy Pier. Já o jardim de inverno e a exposição de vitrais são gratuitos, assim como alguns shows locais que costumam acontecer por lá. O Navy Pier é, ainda, um ponto turístico importante para quem quer comer bem ou fazer compras, já que oferece restaurantes ótimos e lojas interessantes.");
+
+    });
+    $("#a2").click(function () {
+        $("#carousel1").attr("src", "img/LV1.jpg");
+        $("#carousel2").attr("src", "img/LV2.jpg");
+        $("#carousel3").attr("src", "img/LV3.jpg");
+        $("#carousel4").attr("src", "img/LV4.jpg");
+        $("#title").text("Sin City !!!");
+        //$("#imgTitle").attr("src","img/titleLV.png");
+
+        $("#pt1").attr("src", "img/ptlv1.jpg");
+        $("#titlePT1").text("Top of the World at The Stratosphere");
+        $("#pontosTuristicos1").text("O Top of the World é tido como um dos restaurantes mais românticos da cidade. Ele oferece um ambiente para jantar combinado a uma vista estonteante de toda a cidade, tudo isso em uma estrutura 360° que permanece em constante movimento. Sim, é isso mesmo: o restaurante permite que seus visitantes vejam Las Vegas em sua totalidade se eles permanecerem no local por pelo menos 80 minutos, o tempo necessário para uma rotação completa.");
+
+        $("#pt2").attr("src", "img/ptlv2.jpg");
+        $("#titlePT2").text("Mandarin Bar no Mandarin Oriental");
+        $("#pontosTuristicos2").text("Este é um bar que você não quer perder! Com um apelo intimista em um ambiente sofisticado, aproveitar um coquetel aqui o fará se sentir na elite de Vegas. O design chique e a paleta de cores acompanham o ambiente de iluminação baixa. O bar ainda oferece coquetéis artesanais, preparados cuidadosamente pelo seu time de mixologistas.");
+
+        $("#pt3").attr("src", "img/ptlv3.jpg");
+        $("#titlePT3").text("Boulevard Pool no The Cosmopolitan of Las Vegas");
+        $("#pontosTuristicos3").text("Você não encontrará em Las Vegas nenhum outro local como a Boulevard Pool no The Cosmopolitan. É um lugar que se desdobra entre piscina e espaço para eventos. Durante o verão, os visitantes podem curtir e relaxar na piscina. Mas quando ela não está sendo utilizada, o hotel transforma o local em um espaço para shows e até mesmo um ringue de patinação.");
+    });
+    $("#a3").click(function () {
+        $("#carousel1").attr("src", "img/NV1.jpg");
+        $("#carousel2").attr("src", "img/NV2.jpg");
+        $("#carousel3").attr("src", "img/NV3.jpeg");
+        $("#carousel4").attr("src", "img/NV4.jpeg");
+        $("#title").text("The Country City !!!");
+        //$("#imgTitle").attr("src","img/titleNV.png");
+
+        $("#pt1").attr("src", "img/ptnv1.jpeg");
+        $("#titlePT1").text("Museu e Hall of Fame da Música Country");
+        $("#pontosTuristicos1").text("O Museu guarda a história e indústria da música country. Uma caminhada através do museu irá levá-lo de volta no tempo para as raízes da música country, bem como exposições de grandes artista e e mitos do século 21. Certifique-se de ver o Cadillac de ouro de Elvis Presley que está guardado no museu.");
+
+        $("#pt2").attr("src", "img/ptnv2.jpg");
+        $("#titlePT2").text("Auditório Ryman");
+        $("#pontosTuristicos2").text("Um dos marcos mais reconhecidos em Nashville, Tennessee, o Auditório Ryman. Ele se originou em 1982 como um local de culto , mas em 1900, o edifício começou a hospedar eventos seculares, como shows, lutas de boxe e outras atrações. O palco do Ryman foi agraciado por alguns dos maiores nomes da história da música moderna. E apresenta performances ao vivo e eventos semanais e também exposições de memorabilia da história do auditório com mais de 100 anos.");
+
+        $("#pt3").attr("src", "img/ptnv3.jpg");
+        $("#titlePT3").text("Broadway Honky Tonks");
+        $("#pontosTuristicos3").text("A Broadway Honky Tonks centro da cidade de Nashville irá levá-lo de volta a um tempo mais simples quando chapéus e botas de cowboy reinavam supremos. Broadway (como é chamada pelos locais) é o lar de honky-tonk de Nashville.Se você quiser ouvir a música country ao vivo, Broadway é o lugar . E não se esqueça de visitar o mundialmente famoso Tootsies Orchid Lounge. Diz a lenda, que este é o lugar onde os artistas da música country iam depois de seus shows no Ryman.");
+    });
+});
+
+function instagram() {
+    window.open('https://www.instagram.com/?hl=pt-br', '_blank');
+}
+
+function pinterest() {
+    window.open('https://br.pinterest.com/', '_blank');
+}
+
+function twitter() {
+    window.open('https://twitter.com/login?lang=pt', '_blank');
+}
+
+function facebook() {
+    window.open('https://pt-br.facebook.com/', '_blank');
+}
